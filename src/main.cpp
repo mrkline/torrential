4b3505e7f5b15b8b6d1147a621092aeb55d81d80
Justin Krosschell <jkrosschell@wisc.edu>
2014-04-14 11:03:20 -0500

    This is a much better way to write commit messages because vim finally works.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 
-// Lee Stratman added this comment and Justin Krosschell revised it. 
+// Lee Stratman added this comment and Justin Krosschell revised it.
 int main()
 {
 	printf("Hello, ECE 537!\n");
