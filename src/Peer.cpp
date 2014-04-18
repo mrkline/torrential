02c3bb510e8258de7b423f0ccac40b0ceab9a624
Matt Kline <slavik262@gmail.com>
2014-04-18 00:43:50 -0500

    Got things to compile without warnings

--- src/Peer.cpp
+++ src/Peer.cpp
@@ -13,7 +13,12 @@ Connection::Connection (int IP) :
 Peer::Peer (int IP, int upload, int download) :
 	IPAddress(IP),
 	uploadRate(upload),
-	downloadRate(download)
+	downloadRate(download),
+	// These don't need to be in the list, but -WeffC++,
+	// which provides warnings based on Effective C++ (a famous book),
+	// recommends it.
+	chunkList(),
+	connectionList()
 {
 	// don't do anything, but it is cool
 }
--- src/Peer.hpp
+++ src/Peer.hpp
@@ -19,10 +19,12 @@ public:
 class Peer {
 public:
 	// member variables
-	int simCounter;  //peer's simulation counter
+	int simCounter = 0;  //peer's simulation counter
 	int IPAddress;  //peer's IP address
 	int uploadRate;  //peer's upload rate (should be a multiple of 256kB/s)
 	int downloadRate;  //peer's download rate (roughly 10X the upload rate)
 	std::vector<bool> chunkList;  //boolean array list of chunks that the peer has
 	std::vector<Connection> connectionList;  //connection array list of peers that this peer can request chunks from
+
+	Peer(int IP, int upload, int download);
 };
