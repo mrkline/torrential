dacc95e94ab61a8f66d91dbb0759199d55f6e1fc
Matt Kline <slavik262@gmail.com>
2014-05-12 16:41:11 -0500

    Added offer making for each peer

--- src/Peer.cpp
+++ src/Peer.cpp
@@ -4,6 +4,7 @@
 #include "Peer.hpp"
 
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 
@@ -18,6 +19,12 @@ Peer::Peer (int IP, int upload, int download) :
 	interestedList()
 {
 	// don't do anything, but it is cool
+}
+
+bool Peer::hasEverything() const
+{
+	// In the future, we could cache this value, but for now let's focus on correctness
+	return all_of(begin(chunkList), end(chunkList), [](bool b) { return b; });
 }
 
 void Peer::onDisconnect()
@@ -38,4 +45,91 @@ void Peer::reorderPeers()
 	// Zero out the counts
 	for (auto& p : interestedList)
 		p.second = 0;
+}
+
+
+std::vector<std::pair<Peer*, std::vector<size_t>>> Peer::makeOffers() const
+{
+	// Find the rarest chunks among our entire interested list
+
+	// Keeps track of how many of our connected peers (in interestList) have any given chunk
+	// The first item in the pair is the chunk index (because we'll reorder this later)
+	// The second item in the pair is how many peers have that chunk.
+	vector<pair<size_t, int>> popularity(chunkList.size());
+
+	for (size_t i = 0; i < chunkList.size(); ++i)
+		popularity[i].first = i;
+
+	// For each peer we're interested in sharing with
+	for (auto p : interestedList) {
+		const Peer* pp = p.first;
+		assert(pp->chunkList.size() == chunkList.size());
+
+		// Add their chunks to our count
+		for (size_t i = 0; i < chunkList.size(); ++i) {
+			if (pp->chunkList[i])
+				++popularity[i].second;
+		}
+	}
+
+	// Now that we have our counts, remove any that we don't have to offer
+	std::vector<size_t> toRemove; // indices of elements to remove from popularity
+	for (size_t i = 0 ; i < chunkList.size(); ++i) {
+		if (!chunkList[i])
+			toRemove.emplace_back(i);
+	}
+
+	// Remove in reverse order (so that the indices don't change as we go)
+	for (auto it = toRemove.rbegin(); it != toRemove.rend(); ++it) {
+		// Erase takes an iterator, and we can get that from an index, i.e. *it
+		// by adding that index to begin()
+		popularity.erase(popularity.begin() + *it);
+	}
+
+	// Popularity is now a list of chunks we have with their indices and how many peers have them.
+	// Let's sort that by most-to-least needed chunks.
+	sort(begin(popularity), end(popularity), [](const pair<size_t, int>& a, const pair<size_t, int>& b) {
+		return a.second > b.second;
+	});
+
+	// Set up our return value
+	vector<pair<Peer*, vector<size_t>>> ret(min((size_t)4, interestedList.size()));
+
+	// Set up our peer pointers quick
+	for (size_t i = 0; i < 4 && i < interestedList.size(); ++i) {
+		ret[i].first = interestedList[i].first;
+	}
+
+	for (int offered = 0; offered < uploadRate; ++offered) {
+
+		bool gaveSomething = false;
+
+		// For each of our top four peers
+		for (size_t i = 0; i < 4 && i < interestedList.size(); ++i) {
+			Peer* top = interestedList[i].first;
+			assert(ret[i].first == top);
+			vector<size_t>& currentOfferings = ret[i].second;
+
+			if (top->hasEverything())
+				continue; // Take a hike
+
+			// Find the rarest they want that we have and haven't offered yet
+			for (const auto& offering : popularity) {
+				// See if they don't have it and it's not already in our offer list
+				if (!top->chunkList[offering.first] &&
+				    find(begin(currentOfferings), end(currentOfferings), offering.first) == end(currentOfferings)) {
+					// Offer a chunk!
+					currentOfferings.emplace_back(offering.first);
+					gaveSomething = true;
+					break;
+				}
+			}
+		}
+
+		// If nobody wanted our stuff
+		if (!gaveSomething)
+			break;
+	}
+
+	return ret;
 }
--- src/Peer.hpp
+++ src/Peer.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <vector>
+#include <cstddef>
 
 class Peer {
 public:
@@ -13,8 +14,8 @@ public:
 	// member variables
 	int simCounter = 0;  ///< peer's simulation counter
 	const int IPAddress;  ///< peer's IP address
-	const int uploadRate;  ///< peer's upload rate (should be a multiple of 256kB/s)
-	const int downloadRate;  ///< peer's download rate (roughly 10X the upload rate)
+	const int uploadRate;  ///< peer's upload rate in chunks/second
+	const int downloadRate;  ///< peer's download rate in chunks/second (roughly 10X the upload rate)
 	std::vector<bool> chunkList;  ///< boolean array list of chunks that the peer has
 
 	/// Array of peers that this peer can request chunks from and how many chunks they've given us
@@ -24,9 +25,19 @@ public:
 
 	Peer(Peer&&) = default; // Add a move constructor
 
+	bool hasEverything() const;
+
 	/// Called as the peer disconnects to minimize memory footprint when not in use
 	void onDisconnect();
 
 	/// Order peers based on who gave us the most, then reset the counts
 	void reorderPeers();
+
+	/**
+	 * \brief Returns a list of offers to peers from our interestedList
+	 * \returns The offers in the form of pairs.
+	 *          The first item in the pair is the peer we are offering to,
+	 *          and the second item is a list of indices of the chunks
+	 */
+	std::vector<std::pair<Peer*, std::vector<size_t>>> makeOffers() const;
 };
